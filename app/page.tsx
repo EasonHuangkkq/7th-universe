5518667fb9ee0199ca84b1bdd0618a43993d310c
EasonHuang <94727481+EasonHuangkkq@users.noreply.github.com>
2023-06-01 18:11:36 +0800

    add the tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,16 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={styles.main} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100vw', height: '100vh' }}>
+    <main className="flex items-center justify-center w-screen h-screen m-0">
       <iframe 
         src="https://gamma.app/embed/ajv2ye0qs0zazc0" 
-        style={{ width: '100%', height: '100%', position: 'absolute', top: '0', left: '0' }} 
+        className="absolute top-0 left-0 w-screen h-screen"
         allow="fullscreen" 
         title="七号宇宙"
       />
     </main>
   )
 }
-
